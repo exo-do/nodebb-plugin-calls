d3f6d542a2102370294b4fa96fb6f5691b8ea7c8
alberto__segura <alberto.segura.delgado@gmail.com>
2015-04-10 21:05:28 +0200

    send call request only to one user

--- library.js
+++ library.js
@@ -5,7 +5,7 @@ var Topic = module.parent.require('./topics');
 var db = module.parent.require('./database');
 var SocketAdmins = module.parent.require('./socket.io/admin');
 var SocketPlugins = module.parent.require('./socket.io/plugins');
-var websockets = module.parent.require('./socket.io');
+var socket = module.parent.require('./socket.io');
 
 var calls = {};
 
@@ -15,14 +15,14 @@ var calls = {};
   SocketPlugins.callUser = function (socket, data, callback) {
     console.log(socket.uid+" is calling to "+data.uid);
     User.getUserData(socket.uid, function(err, usData){
-      websockets.server.sockets.emit('plugins.incomingCall.'+data.uid, {uid:socket.uid, username:usData.username});
+      socket.in('uid_' + data.uid).emit('plugins.incomingCall.'+data.uid, {uid:socket.uid, username:usData.username});
     });
   };
 
   SocketPlugins.acceptedIncomingCall = function (socket, data, callback) {
     console.log(socket.uid+" accepted call from "+data.youruid);
     User.getUserData(socket.uid, function(err, usData){
-      websockets.server.sockets.emit('plugins.acceptedIncomingCall.'+data.youruid, {peerid:data.peerid, username:usData.username});
+      socket.in('uid_' + data.youruid).emit('plugins.acceptedIncomingCall.'+data.youruid, {peerid:data.peerid, username:usData.username});
     });
   };
 
